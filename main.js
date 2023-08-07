52fb27fa5f0d7c94395f09c8765e4c9874f710f4
always <wpwc2023@gmail.com>
2023-08-08 00:20:44 +0900

    참조형 공부중

--- main.js
+++ main.js
@@ -1,19 +1,12 @@
-let age = null // 명시적
+//Array(배열)
 
-// undefined (암시적)
+const fruits = ['Apple', 'Banana', 'Cherry'] // 리터럴 방식으로 데이터를 만듦
+// new Array('Apple', 'Banana', 'Cherry') //생성자 함수 (js에선 클래스)
 
-console.log(age)
-
-setTimeout(function() {
-    age = 85
-}, 1000)
+console.log(fruits[1]) // Banana 요소 // 인덱싱한다.
+console.log(fruits.length) // 배열의 길이 속성을 알 수 있지.
+console.log(fruits[fruits.length -1 ]) // 길이를 몰라도 마지막 아이템을 알 수 잇다. 
 
 
-const user = {
-    name: 'jena',
-    age: 20
-}
-
-console.log(user)
-console.log(user.name)
-console.log(user.email) // undefined
+// Object(객체)
+const user = new Object()
