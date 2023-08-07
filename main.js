2939da86011d840213348bdae0f164f7be4fd34c
always <wpwc2023@gmail.com>
2023-08-07 22:57:28 +0900

    원시형 공부

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-import _ from 'lodash'
+let age = null // 명시적
+
+// undefined (암시적)
+
+console.log(age)
+
+setTimeout(function() {
+    age = 85
+}, 1000)
 
 
-console.log(_.upperCase('hello-world'))
+const user = {
+    name: 'jena',
+    age: 20
+}
+
+console.log(user)
+console.log(user.name)
+console.log(user.email) // undefined
