d08c78e1af63acedb622f20d89b7bca14f624618
always <wpwc2023@gmail.com>
2023-08-11 00:06:43 +0900

    호이스팅현상 공부

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
-// Do While 반복문
+//함수 (Function)
 
-let n = 0
+//함수 선언문 (Declaration)
+// function hello() {} // function으로 시작하고 이름이있어야하고
 
-// while (n) { .   0은 거짓이라 밑에 문장이 실행안됨
-//     console.log(n)
-// }
 
-do {
-    console.log(n) //조건을 먼저보는게아니라 do 안의 문을 실행 '최초한번'
-} while (n)
+//함수 표현식 (Expression)
+// const hello = function () {} //const, let 등으로 선언된 변수에 할당연산자를 통해 함수가 들어가는지
+
+
+//호이스팅에서 차이가 난다.
+
+//호이스팅(Hoisting)
+
+hello() // 선언됨 
+
+function hello () {
+    console.log('Hello') // -> '함수 선언'이기때문에 자바스크립트로 통해서 가장 꼭대기로 올라가서 동작이되어서 호이스팅현상이 발생한것 : 함수 선언문에서만 발생
+}
+
+hello() //선언됨
+
+///
+hello2() //Error
+
+const hello2 = function() {
+    console.log('함수표현식')
+}
+
+hello2()
