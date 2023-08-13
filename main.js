9777cfb643014898d9b1b1db147680f6eea6dafb
always <wpwc2023@gmail.com>
2023-08-13 21:38:03 +0900

    함수반환 및 종료 연습

--- main.js
+++ main.js
@@ -12,19 +12,34 @@
 
 //호이스팅(Hoisting)
 
-hello() // 선언됨 
+// hello() // 선언됨 
 
-function hello () {
-    console.log('Hello') // -> '함수 선언'이기때문에 자바스크립트로 통해서 가장 꼭대기로 올라가서 동작이되어서 호이스팅현상이 발생한것 : 함수 선언문에서만 발생
-}
+// function hello () {
+//     console.log('Hello') // -> '함수 선언'이기때문에 자바스크립트로 통해서 가장 꼭대기로 올라가서 동작이되어서 호이스팅현상이 발생한것 : 함수 선언문에서만 발생
+// }
 
-hello() //선언됨
+// hello() //선언됨
 
-///
-hello2() //Error
+// ///
+// hello2() //Error
 
-const hello2 = function() {
-    console.log('함수표현식')
-}
+// const hello2 = function() {
+//     console.log('함수표현식')
+// }
 
-hello2()
+// hello2()
+
+
+
+// /** 반환 및 종료 */
+// function plus(num) {
+//     if(typeof num !== 'number' )
+//     console.log('숫자를 입력해주세요~!')
+//     return 0 //함수 종료
+
+//     return num +1 
+// }
+
+// console.log(flus(2)) //3 
+// console.log(flus(7)) //8
+// console.log(plus()) // undefined가 들어가고 거기에 +1 이 더해지면서 숫자이긴 하나 숫자가 아닌 Nan
