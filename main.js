cd46ac6cfd46a47779698bc24258bb286d7a36c7
always <wpwc2023@gmail.com>
2023-08-17 00:00:23 +0900

    Date클래스의 속성, 메소드 공부 유닉스타임 재밌네!

--- main.js
+++ main.js
@@ -617,46 +617,91 @@
 /**Math */
 //Math.abs()
 //주어진 숫자의 절대값을 변환합니다.
-console.log(Math.abs(2))
-console.log(Math.abs(-2)) //2
+// console.log(Math.abs(2))
+// console.log(Math.abs(-2)) //2
 
-//.ceil()
-//주어진 숫자를 올림해 정수를 반환합니다.
-console.log(Math.ceil(3.14)) //4
+// //.ceil()
+// //주어진 숫자를 올림해 정수를 반환합니다.
+// console.log(Math.ceil(3.14)) //4
 
-//.floar()
-//주어진 숫자를 내림처리해 정수를 반환합니다.
-console.log(Math.floor(3.14)) //3
+// //.floar()
+// //주어진 숫자를 내림처리해 정수를 반환합니다.
+// console.log(Math.floor(3.14)) //3
 
-//.max()
-//주어진 숫자들 중 가장 큰 숫자를 반환합니다.
-console.log(Math.max(1,22,34,56)) //56
+// //.max()
+// //주어진 숫자들 중 가장 큰 숫자를 반환합니다.
+// console.log(Math.max(1,22,34,56)) //56
 
-//.min()
-//주어진 숫자들 중 가장 작은 숫자를 반환합니다.
-console.log(Math.min(1,33,45,6,11,-5)) //-5
+// //.min()
+// //주어진 숫자들 중 가장 작은 숫자를 반환합니다.
+// console.log(Math.min(1,33,45,6,11,-5)) //-5
 
-//.pow()
-//주어진 숫자의 거듭제곱한 값을 반환합니다.
-console.log(Math.pow(4,2)) //4의 2제곱
+// //.pow()
+// //주어진 숫자의 거듭제곱한 값을 반환합니다.
+// console.log(Math.pow(4,2)) //4의 2제곱
 
-//.random()
-//숫자 0이상 1미만의 난수를 반환합니다.
-console.log(Math.random()) //0.14121331231~~
+// //.random()
+// //숫자 0이상 1미만의 난수를 반환합니다.
+// console.log(Math.random()) //0.14121331231~~
 
-//특정 범위의 랜덤 정수를 얻는 함수
-function random(min =0, max= 10) {
-    return Math.floor(Math.random()*(max-min) + min)
+// //특정 범위의 랜덤 정수를 얻는 함수
+// function random(min =0, max= 10) {
+//     return Math.floor(Math.random()*(max-min) + min)
+// }
+
+// console.log(random()) // 0~10 사이의 랜덤한 숫자
+// console.log(random(11,20)) //11~20사이의 랜덤한 숫자
+
+// //Math.round()
+// //주어진 숫자를 반올림해 정수를 반환합니다.
+
+// const num4 = 3.14
+// const num5 = 3.71
+
+// console.log(Math.round(num4)) //3
+// console.log(Math.round(num5)) //4
+
+
+/**Date 객체 */
+//.getFullYear()와, .setFullYear()
+
+const date = new Date()
+
+// console.log(date.getFullYear()) //2023
+
+// date.setFullYear(2024)
+// console.log(date.getFullYear) //2024
+// console.log(date) // 2024로 바뀜
+
+//getMonth, setMonth()
+//0부터 시작
+
+console.log(date.getMonth()) //현재월
+console.log(date)
+
+date.setMonth(0) //1월
+console.log(date.getMonth()) //1월
+
+//get(set)Date() : 일을 변환하거나 저장
+//get(set)Hours(): 시간을 변환하거나 저장
+//get(set)Minutes() : '분'을 변환하거나 저장
+//get(set)Seconds() : '초'정보를 변환하거나 저장
+//getDay() : '요일'을 변환한다 / set은 없음 (0:일 1: 월, 2:화, 3:수 ,4:목, 5:금)
+//get(set)Time() 1970-1-1(유닉스타임)부터 경과한 날짜인스턴스의 절대시간을 밀리초로 변환하거나 저장.
+
+console.log(date.getTime()) //1666434830592
+
+Date.prototype.isAfter = function (date) {
+    const a = this.getTime() //d1,d2
+    const b = date.getTime()
 }
 
-console.log(random()) // 0~10 사이의 랜덤한 숫자
-console.log(random(11,20)) //11~20사이의 랜덤한 숫자
+const d1 = new Date('')
+const d2 = new Date('')
 
-//Math.round()
-//주어진 숫자를 반올림해 정수를 반환합니다.
+//Date.now() 정적메소드
+//유닉스타임으로부터 경과한 메소드가 호출될 떄의 '밀리초'로 반환
 
-const num4 = 3.14
-const num5 = 3.71
-
-console.log(Math.round(num4)) //3
-console.log(Math.round(num5)) //4
+const time = new Date().getTime()
+console.log(Date.now()) //즉시
+console.log(time) //고정
